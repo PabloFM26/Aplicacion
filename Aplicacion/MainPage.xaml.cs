b9bdad812489226d2b2f09830d699d95af449917
Pablo Fernández Méndez <pablo.fernandez20@isepceu.es>
2025-01-23 16:53:31 +0100

    Actualización uso de teclado en dispositivos moviles

--- Aplicacion/MainPage.xaml.cs
+++ Aplicacion/MainPage.xaml.cs
@@ -31,6 +31,20 @@
             "Yes",
             "No"))
         {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(translatedNumber);
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                // Other error has occurred.
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
         }
     }
 
